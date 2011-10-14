7ad6663920d0ea9cf3898860c509a134800cedb9
Nils Dagsson Moskopp <nils@dieweltistgarnichtso.net>
2011-10-14 08:42:53 +0200

    + viznut's old sig from the 90s

---
+++ sig.c
@@ -0,0 +1,4 @@
+#include <math.h> /*** ---- viznut --- http://www.hytti.uku.fi/~vheikkil/ */
+main(v,i,z,n,u,t){for(v=-1;;)for(n=pow(/* gcc -lm sig.c; a.out > /dev/dsp */
+1.06,"`cW`g[`cgcg[eYcb^bV^eW^be^bVecb^"[++v&31]+(v&64)/21),i=999;i;putchar(
+128+((8191&u)>i?0:i/8)-((8191&(z+=n))*i-->>16)))u+=v&1?t/2:(t=v&6?t:n/4);}
